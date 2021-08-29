d65c261f2ec7451028e771d11844b5dcc2b222c0
salawum <37109574+salawum@users.noreply.github.com>
2021-08-29 15:05:08 +0100

    Refactored the Numerals class to use dictionaries instead of enums

--- Numeral.py
+++ Numeral.py
@@ -1,5 +1,3 @@
-from enum import Enum
-
 numerals = {
     "one": {"value": 1, "denomination": "ONE"},
     "two": {"value": 2, "denomination": "ONE"},
@@ -10,46 +8,29 @@ numerals = {
     "seven": {"value": 7, "denomination": "ONE"},
     "eight": {"value": 8, "denomination": "ONE"},
     "nine": {"value": 9, "denomination": "ONE"},
+
     "ten": {"value": 10, "denomination": "TEN"},
-    # TODO 
-    # 🙃
+    "eleven": {"value": 11, "denomination": "TEN"},
+    "twelve": {"value": 12, "denomination": "TEN"},
+    "thirteen": {"value": 13, "denomination": "TEN"},
+    "fourteen": {"value": 14, "denomination": "TEN"},
+    "fifteen": {"value": 15, "denomination": "TEN"},
+    "sixteen": {"value": 16, "denomination": "TEN"},
+    "seventeen": {"value": 17, "denomination": "TEN"},
+    "eighteen": {"value": 18, "denomination": "TEN"},
+    "ninteen": {"value": 19, "denomination": "TEN"},
+    "twenty": {"value": 20, "denomination": "TEN"},
+    "thirty": {"value": 30, "denomination": "TEN"},
+    "forty": {"value": 40, "denomination": "TEN"},
+    "fifty": {"value": 50, "denomination": "TEN"},
+    "sixty": {"value": 60, "denomination": "TEN"},
+    "seventy": {"value": 70, "denomination": "TEN"},
+    "eighty": {"value": 80, "denomination": "TEN"},
+    "ninety": {"value": 90, "denomination": "TEN"},
+
+    "hundred": {"value": 100, "denomination": "HUNDRED"},
+
+    "thousand": {"value": 1000, "denomination": "THOUSAND"},
+
+    "million": {"value": 1000000, "denomination": "MILLION"}
 }
-
-class NumeralType(Enum):
-    ONE = 1
-    TEN = 10
-    HUNDRED = 100
-    THOUSAND = 1000
-    MILLION = 1000000
-
-class Numerals(Enum):
-    ONE = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9
-    TEN = 10
-    ELEVEN = 11
-    TWELVE = 12
-    THIRTEEN = 13
-    FOURTEEN = 14
-    FIFTEEN = 15
-    SIXTEEN = 16
-    SEVENTEEN = 17
-    EIGHTEEN = 18
-    NINETEEN = 19
-    TWENTY = 20
-    THIRTY = 30
-    FORTY = 40
-    FIFTY = 50
-    SIXTY = 60
-    SEVENTY = 70
-    EIGHTY = 80
-    NINETY = 90
-    HUNDRED = 100
-    THOUSAND = 1000
-    MILLION = 1000000
